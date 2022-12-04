f36a49d40c685b5b972defea4b92e6f1b363ca4e
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-04 13:12:36 +0100

    optimize 04

--- 2022/04.py
+++ 2022/04.py
@@ -18,6 +18,11 @@ part2 = 0
 for a in assignments:
     range1, range2 = a
 
-    if (range1[0] >= range2[0] and range1[0] <= range2[1]) or (range1[1] >= range2[0] and range1[1] <= range2[1]) or (range2[0] >= range1[0] and range2[0] <= range1[1]) or (range2[1] >= range1[0] and range2[1] <= range1[1]):
+    # if (range1[0] >= range2[0] and range1[0] <= range2[1]) or (range1[1] >= range2[0] and range1[1] <= range2[1]) or (range2[0] >= range1[0] and range2[0] <= range1[1]) or (range2[1] >= range1[0] and range2[1] <= range1[1]):
+    #     part2 += 1
+
+    # optimization: https://stackoverflow.com/a/325964/5284327
+    if range1[1] >= range2[0] and range1[0] <= range2[1]:
         part2 += 1
+
 print("part2:", part2)
