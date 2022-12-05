37879377289b52436a3fc33035535031cf3f3a3a
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-05 21:05:50 +0100

    [22-04] merge loops

--- 2022/04.py
+++ 2022/04.py
@@ -5,24 +5,19 @@ assignments = [[list(map(int, y.split("-")))
 
 
 part1 = 0
+part2 = 0
 for a in assignments:
     range1, range2 = a
 
     if (range1[0] <= range2[0] and range1[1] >= range2[1]) or (range2[0] <= range1[0] and range2[1] >= range1[1]):
         part1 += 1
 
-
-print("part1:", part1)
-
-part2 = 0
-for a in assignments:
-    range1, range2 = a
-
     # if (range1[0] >= range2[0] and range1[0] <= range2[1]) or (range1[1] >= range2[0] and range1[1] <= range2[1]) or (range2[0] >= range1[0] and range2[0] <= range1[1]) or (range2[1] >= range1[0] and range2[1] <= range1[1]):
     #     part2 += 1
-
     # optimization: https://stackoverflow.com/a/325964/5284327
     if range1[1] >= range2[0] and range1[0] <= range2[1]:
         part2 += 1
 
+
+print("part1:", part1)
 print("part2:", part2)
