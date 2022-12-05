a71bb7116b34935056b14a7a4cd8f6ee49a435eb
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-05 21:06:10 +0100

    [22-05] also parse stacks

--- 2022/05.py
+++ 2022/05.py
@@ -1,50 +1,28 @@
-'''
-                    [L]     [H] [W]
-                [J] [Z] [J] [Q] [Q]
-[S]             [M] [C] [T] [F] [B]
-[P]     [H]     [B] [D] [G] [B] [P]
-[W]     [L] [D] [D] [J] [W] [T] [C]
-[N] [T] [R] [T] [T] [T] [M] [M] [G]
-[J] [S] [Q] [S] [Z] [W] [P] [G] [D]
-[Z] [G] [V] [V] [Q] [M] [L] [N] [R]
- 1   2   3   4   5   6   7   8   9 
-'''
-
 import sys
 from copy import deepcopy
 
-moves = [list(map(int, x.split()[1::2])) for x in sys.stdin.readlines()]
+crates, moves = sys.stdin.read().split("\n\n")
 
-crates = list(map(list, [
-    "ZJNWPS",
-    "GST",
-    "VQRLH",
-    "VSTD",
-    "QZTDBMJ",
-    "MWTJDCZL",
-    "LPMWGTJ",
-    "NGMTBFQH",
-    "RDGCPBQW",
-]))
+crates = crates.split("\n")[:-1]
+crates = [c[1::4] for c in crates]
+# transpose and filter https://scipython.com/book/chapter-4-the-core-python-language-ii/examples/the-matrix-transpose-by-list-comprehension/) 
+crates.reverse()
+crates = [[row[i] for row in crates if row[i] != " "] for i in range(len(crates) + 1)]  
+
+moves = [list(map(int, x.split()[1::2])) for x in moves.strip().split("\n")]
 
 part1 = deepcopy(crates)
-
 for m in moves:
     amount, move_from, move_to = m
-
     for a in range(amount):
         tmp = part1[move_from-1].pop()
         part1[move_to-1].append(tmp)
-
 print("part1:", "".join(list(map(lambda stack: stack[-1], part1))))
 
 
 part2 = deepcopy(crates)
-
 for m in moves:
     amount, move_from, move_to = m
-
     part2[move_to-1].extend(part2[move_from-1][-amount:])
     part2[move_from-1] = part2[move_from-1][:-amount]
-
 print("part2:", "".join(list(map(lambda x: x[-1], part2))))
