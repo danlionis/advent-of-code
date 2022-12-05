6904f37d786f3958a70d97a73ed220562a669223
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-05 21:57:09 +0100

    [22-05] optimization

--- 2022/05.py
+++ 2022/05.py
@@ -5,24 +5,27 @@ crates, moves = sys.stdin.read().split("\n\n")
 
 crates = crates.split("\n")[:-1]
 crates = [c[1::4] for c in crates]
-# transpose and filter https://scipython.com/book/chapter-4-the-core-python-language-ii/examples/the-matrix-transpose-by-list-comprehension/) 
+# transpose and filter https://stackoverflow.com/a/6473724/5284327
 crates.reverse()
-crates = [[row[i] for row in crates if row[i] != " "] for i in range(len(crates) + 1)]  
+crates = [list(filter(lambda x: x != " ", t)) for t in zip(*crates)]
 
 moves = [list(map(int, x.split()[1::2])) for x in moves.strip().split("\n")]
 
 part1 = deepcopy(crates)
-for m in moves:
-    amount, move_from, move_to = m
-    for a in range(amount):
-        tmp = part1[move_from-1].pop()
-        part1[move_to-1].append(tmp)
-print("part1:", "".join(list(map(lambda stack: stack[-1], part1))))
-
-
 part2 = deepcopy(crates)
+x = len(moves)
+i = 0
 for m in moves:
+    print(i, x,  m)
+    i += 1
     amount, move_from, move_to = m
+
+    part1[move_to-1].extend(part1[move_from-1][-amount:][::-1])
+    part1[move_from-1] = part1[move_from-1][:-amount]
+
     part2[move_to-1].extend(part2[move_from-1][-amount:])
     part2[move_from-1] = part2[move_from-1][:-amount]
+
+print("part1:", "".join(list(map(lambda stack: stack[-1], part1))))
 print("part2:", "".join(list(map(lambda x: x[-1], part2))))
+
