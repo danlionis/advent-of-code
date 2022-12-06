8e194e3798c0bf66c9c33b4f1c682603bb2429d5
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-06 11:17:46 +0100

    [22-06] add oneline solutions

--- 2022/06.py
+++ 2022/06.py
@@ -14,3 +14,7 @@ def solve(data, n):
 
 print("part1:", solve(datastream, 4))
 print("part2:", solve(datastream, 14))
+
+# oneliners
+print("part1:", [len(set(datastream[i:i+4])) for i in range(len(datastream))].index(4) + 4)
+print("part2:", [len(set(datastream[i:i+14])) for i in range(len(datastream))].index(14) + 14)
