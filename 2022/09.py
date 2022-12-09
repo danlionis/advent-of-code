595785bac8a68bef68f4d1284042ea78befdcb96
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-09 13:43:31 +0100

    [22-09] refactor + visualization

--- 2022/09.py
+++ 2022/09.py
@@ -1,4 +1,5 @@
 import sys
+import time
 
 data = sys.stdin.read().splitlines()
 
@@ -21,6 +22,21 @@ position_tail = (0, 0)
 move_deltas = {"U": (0, 1), "R": (1, 0), "D": (0, -1), "L": (-1, 0)}
 
 
+def visualize(knots):
+    time.sleep(1/100)
+    max_x = 100
+    max_y = 60
+
+    knots = set(knots)
+
+    print(chr(27) + "[2J")
+
+    for y in range(max_y):
+        line = ["#" if (x - (max_x // 2), y  - (max_y // 2))
+                in knots else "." for x in range(max_x)]
+        print("".join(line))
+
+
 def sign(x):
     return 1 if x >= 0 else -1
 
@@ -34,8 +50,6 @@ def update_tail(head, tail):
 
     delta_x = head_x - tail_x
     delta_y = head_y - tail_y
-
-    # print(head, tail, delta_x, delta_y, sign(delta_x))
 
     match (abs(delta_x), abs(delta_y)):
         case 0, 0: return tail
@@ -51,34 +65,23 @@ def update_tail(head, tail):
     print("invalid state", head, tail, delta_x, delta_y)
     return tail
 
+def solve(moves, knot_amount: int):
+    knots = [(0, 0)] * knot_amount
+    res = set()
+    for direction, amount in moves:
+        for i in range(amount):
+            ds = move_deltas[direction]
+            head = knots[0]
+            knots[0] = (head[0] + ds[0], head[1] + ds[1])
 
-part1 = set()
+            for i in range(len(knots) - 1):
+                tmp_head = knots[i]
+                tmp_tail = knots[i + 1]
+                knots[i + 1] = update_tail(tmp_head, tmp_tail)
 
-for direction, amount in moves:
-    for i in range(amount):
-        ds = move_deltas[direction]
-        position_head = (position_head[0] + ds[0], position_head[1] + ds[1])
-        position_tail = update_tail(position_head, position_tail)
-        part1.add(position_tail)
+            visualize(knots)
+            res.add(knots[-1])
+    return len(res)
 
-print("part1:", len(part1))
-
-
-knots = [(0, 0)] * 10
-
-part2 = set()
-
-for direction, amount in moves:
-    for i in range(amount):
-        ds = move_deltas[direction]
-        head = knots[0]
-        knots[0] = (head[0] + ds[0], head[1] + ds[1])
-
-        for i in range(len(knots) - 1):
-            tmp_head = knots[i]
-            tmp_tail = knots[i + 1]
-            knots[i + 1] = update_tail(tmp_head, tmp_tail)
-
-        part2.add(knots[-1])
-
-print("part2:", len(part2))
+print("part1:", solve(moves, 2))
+print("part2:", solve(moves, 10))
