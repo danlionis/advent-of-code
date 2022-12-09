60b96cbede809c5bed6f8c1daa88bec78171300b
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-09 11:46:29 +0100

    [22-09] part 2

--- 2022/09.py
+++ 2022/09.py
@@ -1,6 +1,6 @@
 import sys
 
-data = sys.stdin.read().splitlines();
+data = sys.stdin.read().splitlines()
 
 # data = """R 4
 # U 4
@@ -46,8 +46,11 @@ def update_tail(head, tail):
         case 0, 2: return (tail_x, tail_y + sign(delta_y))
         case 2, 1: return (tail_x + sign(delta_x), tail_y + sign(delta_y))
         case 1, 2: return (tail_x + sign(delta_x), tail_y + sign(delta_y))
+        case 2, 2: return (tail_x + sign(delta_x), tail_y + sign(delta_y))
 
     print("invalid state", head, tail, delta_x, delta_y)
+    return tail
+
 
 part1 = set()
 
@@ -58,7 +61,24 @@ for direction, amount in moves:
         position_tail = update_tail(position_head, position_tail)
         part1.add(position_tail)
 
-print(len(part1))
+print("part1:", len(part1))
 
 
-print(update_tail((2, 1), (0, 0)))
+knots = [(0, 0)] * 10
+
+part2 = set()
+
+for direction, amount in moves:
+    for i in range(amount):
+        ds = move_deltas[direction]
+        head = knots[0]
+        knots[0] = (head[0] + ds[0], head[1] + ds[1])
+
+        for i in range(len(knots) - 1):
+            tmp_head = knots[i]
+            tmp_tail = knots[i + 1]
+            knots[i + 1] = update_tail(tmp_head, tmp_tail)
+
+        part2.add(knots[-1])
+
+print("part2:", len(part2))
