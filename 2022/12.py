f58525ea1fe4a5a4d5344dcfea3068fb518d2dfb
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-12 20:52:17 +0100

    [22-12] make single-pass

--- 2022/12.py
+++ 2022/12.py
@@ -1,18 +1,24 @@
 import sys
 
+
 def get_height_value(letter):
-    if letter == "S": letter = "a"
-    if letter == "E": letter = "z"
+    if letter == "S":
+        letter = "a"
+    if letter == "E":
+        letter = "z"
 
     return ord(letter) - ord("a")
 
+
+# top, right, bottom, left
+deltas = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
 data = sys.stdin.read().splitlines()
 heightmap = [[get_height_value(letter) for letter in line] for line in data]
-# print(heightmap)
 
+# find start and end locations
 start = (0, 0)
 end = (0, 0)
-
 for y in range(len(data)):
     for x in range(len(data[0])):
         if data[y][x] == "S":
@@ -20,27 +26,15 @@ for y in range(len(data)):
         if data[y][x] == "E":
             end = (x, y)
 
-steps = [[0] * len(heightmap[0]) for _ in range(len(heightmap))]
-# print(steps)
-
-# top, right, bottom, left
-deltas = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+visited = [[0] * len(heightmap[0]) for _ in range(len(heightmap))]
 
 
-for y in range(len(heightmap)):
-    for x in range(len(heightmap[0])):
-        if heightmap[y][x] == 1000:
-            start = (x, y)
-        if heightmap[y][x] == -1:
-            end = (x, y)
-
-def get_adjecent(current_pos):
-    cur_x, cur_y = current_pos
+def get_neighbors(x, y):
 
     adjecent = []
 
     for dx, dy in deltas:
-        next_x, next_y = cur_x + dx , cur_y + dy
+        next_x, next_y = x + dx, y + dy
 
         # skip if out of bounds
         if next_x < 0 or next_x >= len(heightmap[0]) or next_y < 0 or next_y >= len(heightmap):
@@ -49,43 +43,30 @@ def get_adjecent(current_pos):
 
     return adjecent
 
+
 queue = []
-queue.append(start)
+queue.append(end)  # go from end to start to make the whole process one-pass
+
+part1, part2 = 0, 0
+
 while len(queue) > 0:
-    current_pos = queue.pop(0)
-    cur_x, cur_y = current_pos
-    cur_height = heightmap[cur_y][cur_x]
+    x, y = queue.pop(0)
+    cur_height = heightmap[y][x]
 
-    if current_pos == end:
-        print("part1:", steps[cur_y][cur_x])
-        break
-
-    for next_x, next_y in get_adjecent(current_pos):
-        next_height = heightmap[next_y][next_x]
-
-        if steps[next_y][next_x] == 0:
-            if next_height <= cur_height + 1:
-                queue.append((next_x, next_y))
-                steps[next_y][next_x] = steps[cur_y][cur_x] + 1
-
-
-# same thing but backwards, we just look for the first elevation of "a" doing a BFS from the end
-steps = [[0] * len(heightmap[0]) for _ in range(len(heightmap))]
-queue = []
-queue.append(end)
-while len(queue) > 0:
-    current_pos = queue.pop(0)
-    cur_x, cur_y = current_pos
-    cur_height = heightmap[cur_y][cur_x]
+    if (x, y) == start:
+        part1 = visited[y][x]
 
     if cur_height == 0:
-        print("part2:", steps[cur_y][cur_x])
-        break
+        if part2 == 0:  # only set once on first occurence of height "a"
+            part2 = visited[y][x]
 
-    for next_x, next_y in get_adjecent(current_pos):
+    for next_x, next_y in get_neighbors(x, y):
         next_height = heightmap[next_y][next_x]
 
-        if steps[next_y][next_x] == 0:
+        if visited[next_y][next_x] == 0:  # if not visited
             if next_height >= cur_height - 1:
                 queue.append((next_x, next_y))
-                steps[next_y][next_x] = steps[cur_y][cur_x] + 1
+                visited[next_y][next_x] = visited[y][x] + 1
+
+print("part1:", part1)
+print("part2:", part2)
