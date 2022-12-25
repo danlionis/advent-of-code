52f1caf535ce610f3cd1664aaa7311835e9dac0b
Dan Lionis <26198474+danlionis@users.noreply.github.com>
2022-12-25 15:27:51 +0100

    [22-16] part1

--- 2022/16.py
+++ 2022/16.py
@@ -8,14 +8,47 @@ def process_line(line):
     return (valve, {"v": valve, "f": flow_rate, "t": tunnels})
 
 
-data = dict(map(process_line, sys.stdin.read().strip().splitlines()))
-# print(data)
+valves = dict(map(process_line, sys.stdin.read().strip().splitlines()))
 
-# print(sorted([(k, v) for k, v in data.items() if v["f"] > 0]))
+max_score = 0
 
-opened = set()
-current_valve = "AA"
+def not_opened(opened):
+    return [x for x in valves.keys() if x not in opened]
 
-# for i in range(30, 0, -1):
+def best_possible_score(opened, minute):
+    vs = not_opened(opened)
+    flowrates = sorted([valves[v]["f"] for v in vs], reverse=True)
 
-#     should_open =
+    return sum([a * b for a, b in zip(flowrates, range(minute, 0, -2))])
+
+
+def move(valve: str, minute: int, score: int, opened: list):
+    # print(31 - minute, valve, opened)
+    global max_score
+    flow = valves[valve]["f"]
+    tunnels = valves[valve]["t"]
+
+
+    if minute <= 0 or len(opened) == len(valves):
+        # print(opened, score, max_score)
+        max_score = max(score, max_score)
+        return
+
+    if best_possible_score(opened, minute) + score <= max_score:
+        # print("not feasable", minute, opened)
+        return
+
+    # open valve
+
+    if flow > 0 and valve not in opened:
+        new_score = score + (minute - 1) * flow
+        new_opened = opened + [valve]
+    
+        for tunnel in tunnels:
+            move(tunnel, minute - 2, new_score, new_opened)
+
+    for tunnel in tunnels:
+        move(tunnel, minute - 1, score, opened)
+
+move("AA", 30, 0, [])
+print("part1:", max_score)
